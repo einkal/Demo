1f5845db6ae1f23ea689fb1a429d8feda0a98cd9
Amrita Singh <einkal4@gmail.com>
2018-05-14 19:28:00 +0530

    Skill

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-
-
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+import { AppRoutingModule }     from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent }         from './app.component';
+import {LoginComponent} from './login/login.component';
+import {User} from './user';
+import {Profile} from './profile';
+import {SearchComponent} from './search/search.component';
+import {RegisterComponent} from './register/register.component';
+import {LoginService} from "./login.service";
+import {UserService} from "./user.service";
+import { ListuserComponent } from './listuser/listuser.component';
+import {UserhomeComponent} from './userhome/userhome.component';
+import { ApprovalComponent } from './approval/approval.component';
+import { ResumebuilderComponent } from './resumebuilder/resumebuilder.component';
+import { ResumeComponent } from './resume/resume.component';
+import { LogoutComponent } from './logout/logout.component';
+import { MentorsComponent } from './mentors/mentors.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UpdateprofileComponent } from './updateprofile/updateprofile.component';
+import { CreateprofileComponent } from './createprofile/createprofile.component';
+import { ImageuploadComponent } from './imageupload/imageupload.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule],
+  declarations: [
+    AppComponent,LoginComponent,RegisterComponent,SearchComponent,UserhomeComponent, ListuserComponent, ApprovalComponent, ResumebuilderComponent, ResumeComponent, LogoutComponent, MentorsComponent, ProfileComponent, UpdateprofileComponent, CreateprofileComponent, ImageuploadComponent
+  
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [LoginService,UserService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
